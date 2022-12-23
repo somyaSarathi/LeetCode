9c3ea298112e78e3a376b6c70e6e565f8b570a09
Somya Sarathi Samal <soms9edu@gmail.com>
2022-12-23 18:23:49 +0530

    Updating solution to LeetCode problem #150     + 150_evaluate_reverse_polish_notation.py

---
+++ Solutions/150_evaluate_reverse_polish_notation.py
@@ -0,0 +1,30 @@
+class Solution:
+    def evalRPN(self, tokens: list[str]) -> int:
+        operators = {
+            '+': lambda a, b: int(a)+int(b),
+            '-': lambda a, b: int(a)-int(b),
+            '*': lambda a, b: int(a)*int(b),
+            '/': lambda a, b: int(a)/int(b)
+        }
+
+        i = 0
+        while i < len(tokens):
+            if tokens[i] in operators:
+                tokens[i-2] = operators[tokens[i]](tokens[i-2], tokens[i-1])
+
+                i -= 2
+
+                tokens.pop(i+1)
+                tokens.pop(i+1)
+
+            i += 1
+
+        return int(tokens[0])
+
+
+
+if __name__ == '__main__':
+    tokens = ["18"]
+    print(Solution().evalRPN(tokens))
+
+    exit(0)
